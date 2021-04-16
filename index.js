45fbc4ba4adae11d448ece3543b8706a28a7b35c
quinn5moli <quinn.moli@gmail.com>
2021-04-15 21:22:18 -0700

    on last task

--- index.js
+++ index.js
@@ -41,28 +41,28 @@ class Airplane {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- class Person {
-    constructor(name, age) {
-      this.name = name;
-      this.age = age;
-      this.stomach = [];
-    }
-    
-    speak(someFood){
-      if (this.stomach.length < 11) {
-        this.stomach.push(someFood)
-      }
-      return this.stomach
-    }
-
-    poop(){
-      this.stomach= []
-    }
-
-    toString(){
-      return `${this.name},${this.age}`
-    }
-  }
+          class Person {
+            constructor(name, age) {
+              this.name = name;
+              this.age = age;
+              this.stomach = []
+            }
+        
+            eat(someFood) {
+              if (this.stomach.length < 10) {
+                this.stomach.push(someFood);
+                }
+                return this.stomach;
+            }
+        
+            poop() {
+              this.stomach = [];
+            }
+        
+            toString() {
+              return `${this.name}, ${this.age}`
+            }
+          }        
   
   /*
     TASK 2
@@ -78,31 +78,34 @@ class Airplane {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- class Car {
-   constructor(model,milesPerGallon){
-     this.model = model;
-     this.milesPerGallon;
-     this.tank= 0;
-     this.odometer= 0;
-   }
-
-    fill(gallons){
-      return this.tank = this.tank + gallons;
-    }
-
-    drive(distance){
-      let distToEmpty = this.tank * this.milesPerGallon;
-
-      for(let i = distance; i>0; i--)
-      distToEmpty -= 1;
-      this.odometer += 1;
-      this.tank = distToEmpty/this.milesPerGallon;
-        if (this.tank = 0){
-          console.log(`I ran out of fuel at ${this.odometer} miles!`)
-          return `I ran out of fuel at ${this.odometer} miles!`
-        }
-    }
-  }
+          class Car {
+            constructor(model, milesPerGallon) {
+              this.model = model,
+              this.milesPerGallon = milesPerGallon;
+              this.tank = 0,
+              this.odometer = 0
+            }
+        
+            fill(gallons) {
+              this.tank += gallons;
+              return this.tank;
+            }
+        
+            drive(distance) {
+              let distToEmpty = this.tank * this.milesPerGallon;
+              
+              for (let i = distance; i > 0; i-- ) {        
+                distToEmpty -= 1;
+                this.odometer += 1;
+                this.tank = distToEmpty/this.milesPerGallon;
+                if (this.tank <= 0) {
+                  console.log(`I ran out of fuel at ${this.odometer} miles!`)
+                  return `I ran out of fuel at ${this.odometer} miles!`
+                }
+              }
+        
+            }
+          }  
   
   /*
     TASK 3
@@ -116,9 +119,16 @@ class Airplane {
           + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
           + {name} and {location} of course come from the instance's own properties.
   */
- class Lambdasian {
-    
-  }
+          class Lambdasian {
+            constructor({name, age, location}) {
+              this.name = name,
+              this.age = age,
+              this.location = location
+            }
+            speak() {
+              return `Hello my name is ${this.name}, I am from ${this.location}`
+            }
+          }
   
   /*
     TASK 4
@@ -134,9 +144,22 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
-
- }
+          class Instructor extends Lambdasian {
+            constructor(attr) {
+              super(attr);
+              this.specialty = attr.specialty;
+              this.favLanguage = attr.favLanguage;
+              this.catchPhrase = attr.catchPhrase;
+            }
+        
+            demo(subject) {
+              return `Today we are learning about ${subject}`
+            }
+        
+            grade(student, subject) {
+              return `${student.name} receives a perfect score on ${subject}`
+            }
+         }
   /*
     TASK 5
       - Write a Student class extending Lambdasian.
@@ -152,9 +175,27 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
-     
- }
+          class Student extends Lambdasian {
+            constructor(attr) {
+              super(attr);
+              this.previousBackground = attr.previousBackground;
+              this.className = attr.className;
+              this.favSubjects = attr.favSubjects;
+            }
+         
+            listSubjects() {
+              return `Loving ${this.favSubjects}!`;
+            }
+         
+            PRAssignment(subject) {
+              return `${this.name} has submitted a PR for ${subject}`
+            }
+         
+            sprintChallenge(subject) {
+              return `${this.name} has begun sprint challenge on ${subject}`
+            }
+              
+          }
   
   /*
     TASK 6
